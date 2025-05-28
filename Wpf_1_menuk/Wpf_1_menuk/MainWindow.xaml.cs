f63f4d559f3b623ca4ff0f03bd929f03cb748745
helyikan12 <helyikan2005@gmail.com>
2025-05-28 08:23:20 +0200

    Add files via upload

---
+++ Wpf_1_menuk/Wpf_1_menuk/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_1_menuk
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void New_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Új fájlt hoztál létre");
+        }
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        private void About_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Ez egy egyszerű WPF alkalmazás", "Névjegy");
+        }
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Az első gombot nyomtad meg");
+        }
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("A második gombot nyomtad meg");
+        }
+        private void Copy_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Másolás");
+        }
+        private void Paste_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Beillesztés");
+        }
+    }
+}
